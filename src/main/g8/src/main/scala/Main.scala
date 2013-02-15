9512317ca31818f2a28b0c24077105c73a596c60
Mark Schaake <mark.schaake@gmail.com>
2013-02-14 16:29:23 -0800

    Fix compile error

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -2,8 +2,10 @@ import spray.routing.SimpleRoutingApp
 import twirl.api._
 import spray.httpx.TwirlSupport
 import spray.httpx.encoding.Gzip
+import spray.can.server.SprayCanHttpServerApp
 
 object Main extends App
+    with SprayCanHttpServerApp
     with SimpleRoutingApp
     with TwirlSupport
     with spray.httpx.SprayJsonSupport {
