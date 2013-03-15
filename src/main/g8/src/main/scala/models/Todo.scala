dec612da0c99d7d2e8f2c9bc1a6cd5fcdbf108ef
Mark Schaake <mark.schaake@gmail.com>
2013-03-15 00:21:04 -0700

    another fix

--- src/main/g8/src/main/scala/models/Todo.scala
+++ src/main/g8/src/main/scala/models/Todo.scala
@@ -6,6 +6,6 @@ import sprest.models._
 case class Todo(text: String, done: Boolean, var id: Option[String] = None) extends Model[String]
 
 object Todo extends ModelCompanion[Todo, String] {
-  implicit val JsonFormat = jsonFormat3(Todo)
+  implicit val JsonFormat = jsonFormat3(Todo.apply _)
   implicit val BsonFormat = generateBSONFormat(JsonFormat)
 }
