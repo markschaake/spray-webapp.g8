cc82edca3c66407401d4f834b7d2660077826aa6
Mark Schaake <mark.schaake@gmail.com>
2013-03-16 21:51:19 -0700

    Update template for new sprest api

--- src/main/g8/src/main/scala/models/Todo.scala
+++ src/main/g8/src/main/scala/models/Todo.scala
@@ -6,6 +6,5 @@ import sprest.models._
 case class Todo(text: String, done: Boolean, var id: Option[String] = None) extends Model[String]
 
 object Todo extends ModelCompanion[Todo, String] {
-  implicit val JsonFormat = jsonFormat3(Todo.apply _)
-  implicit val BsonFormat = generateBSONFormat(JsonFormat)
+  implicit val jsonFormat = jsonFormat3(Todo.apply _)
 }
